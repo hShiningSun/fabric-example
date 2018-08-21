ef1aef2c6fcf7e7a63fbd9837666de60dcfce453
yc.Hou <522245189@qq.com>
2018-08-21 21:04:45 +0800

    初始化结构

---
+++ changeCli.sh
@@ -0,0 +1,8 @@
+CORE_PEER_ID=peer0.zhangfeilong.example.com
+CORE_PEER_ADDRESS=peer0.zhangfeilong.example.com:7051
+CORE_PEER_CHAINCODELISTENADDRESS=peer0.zhangfeilong.example.com:7052
+CORE_PEER_GOSSIP_EXTERNALENDPOINT=peer0.zhangfeilong.example.com:7051
+CORE_PEER_LOCALMSPID=zhangfeilongMSP
+
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/zhangfeilong.example.com/peers/peer0.zhangfeilong.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/zhangfeilong.example.com/users/Admin@zhangfeilong.example.com/msp
